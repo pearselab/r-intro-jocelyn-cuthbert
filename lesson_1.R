83b2fefdd67d89671beaf528d30c1a7f744984e1
jocelyn-cuthbert <jcuthber@aggiemail.usu.edu>
2016-10-19 11:47:54 -0600

    addition of usage

--- lesson_1.R
+++ lesson_1.R
@@ -9,12 +9,9 @@ c(c(1,2,3), c(4,5,6))
 #     - they need to be of the same type, though!
 c(1,2, "three")
 
-
-<<<<<<< HEAD
 #cat - concatenate and print:  Outputs the objects, but with less conversions than print, which makes it useful for producing output in user-defined functions. Converts argument to character vectors, concatenates them to a single character vector. 
-=======
-#cat - concatenate and print:  Outputs the objects, but with less conversions than print. 
->>>>>>> 736fa41f9d10bc4679235a31dba5daed7914b936
+cat(... , file = "", sep = " ", fill = FALSE, labels = NULL,
+    append = FALSE)
 
 #c-bind - 
 
