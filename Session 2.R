c0c684eca6ca20477352afbe35f707c9f68049a5
jocelyn-cuthbert <jcuthber@aggiemail.usu.edu>
2016-10-24 11:53:39 -0600

    #4 with a function struggles
    have 3 functions for prime numbers - created a specific function for
    #4, where it would only put JOB:NUMBER if it was prime and nothing if
    it was not prime.

--- Session 2.R
+++ Session 2.R
@@ -13,8 +13,21 @@ for (i in 20:10)
 }
 
 #3. Write a function that calculates whether a number is a prime number
-num = as.integer (10)
 
+is_prime <- function (num) {
+  if (num == 2) {
+    TRUE
+  } else if (any(num%%2:(num-1)==0)) {# see example function of a "any
+    FALSE
+  } else {
+      TRUE
+    }
+  }
+
+is_prime(7)
+
+
+is_prime2 <-function (num) {
 flag = 0
 if (num >1) {
   flag = 1
@@ -32,32 +45,40 @@ if(flag ==1) {
 } else {
     print(paste(num,"is not a prime number"))
 }
+}
+is_prime2(7)
 
 #4. Write a loop that prints out the numbers from 1 to 20, printing "Good:NUMBER" if the number is divisible by five
 #and "Job: NUMBER" if the number is prime, and nothing otherwise. 
-for (i in 1:20) 
-  if (i %% 5==0) {
+is_prime3 <-function (num) {
+  flag = 0
+  if (num >1) {
+    flag = 1
+    for (i in 2:(num-1)) {
+      if ((num %% i) == 0) {
+        flag = 0
+        break
+      }
+    }
+  }
+  
+  if(num== 2)  flag = 1
+  if(flag ==1) {
+    print(paste(num,"Job:NUMBER"))
+  } 
+}
+
+is_prime3(7)
+
+for (num in 1:20) {
+  if (num %% 5==0) {
     print ("Good:NUMBER")
   } else {
-    if (i)
-        flag = 0
-        if (num >1) {
-          flag = 1
-          for (i in 2:(num-1)) {
-            if ((num %% i) == 0) {
-              flag = 0
-              break
-            }
-          }
-        }
+    is_prime3(num)  
+  } else {
+    break
   }
-        
-        if(num== 2)  flag = 1
-        if(flag ==1) {
-          print(paste("Job:NUMBER"))
-        } else {
-          break
-        }
+}
 
 #5. 
 
