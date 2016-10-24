24e7f728989a7e20f73e15217d6202bce78aa975
jocelyn-cuthbert <jcuthber@aggiemail.usu.edu>
2016-10-24 09:47:36 -0600

    Questions for Session 2

---
+++ Session 2.R
@@ -0,0 +1,12 @@
+#Session 2
+#Loop exercises in handout
+
+#1.  Write a loop that prints out the numbers from 20 to 10
+
+
+#2. Write a loop that prints out only the numbers from 20 to 10 that are even
+
+#3. Write a function that calculates whether a number is a prime number
+
+#4. Write a loop that prints out the numbers form 1 to 20, printing "Good:NUMBER" if the number is divisible by five
+#and "Job: NUMBER" if the number in prime, and nothing otherwise. 
